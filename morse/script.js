b4bac99a964398739c324c22b90ab3f04bc87b78
Amos von Wyl <vonwylamos@gmail.com>
2023-09-19 22:59:30 +0200

    update morse tool

--- morse/script.js
+++ morse/script.js
@@ -1,5 +1,6 @@
 var translation_string = "";
 var translated_morse = "";
+
 const dictionary_A = {
   A: ".-",
   B: "-...",
@@ -68,6 +69,7 @@ const dictionary_B = {
   "--..": "z",
   "/": " ",
 };
+
 function MorseToAlpha() {
   //document.A_Form.alpha.value = "";
   //document.getElementById("A_Form").reset();
@@ -99,4 +101,38 @@ function AlphaToMorse() {
   console.log(translation_string);
   document.getElementById("morse").value = translation_string.trim();
   translation_string = "";
+}
+
+var slider_sign = document.getElementById("sign");
+var slider_word = document.getElementById("word");
+var output_sign = document.getElementById("sign_value");
+var output_word = document.getElementById("word_value");
+var sign_pause = 500;
+var word_pause = 1000;
+output_sign.innerHTML = display_value(slider_sign, output_sign, sign_pause);
+output_word.innerHTML = display_value(slider_word, output_word, word_pause);
+
+function display_value(slider, output, pause) {
+  slider.oninput = function () {
+    output.innerHTML = this.value;
+    pause = slider.value;
+  };
+}
+
+function generate_audio() {
+  let every_sign = new Array();
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("C4", 2);
+
+  morse = document.M_Form.morse.value;
+
+  morse_words = morse.split(" "); //creates an array with all of the "words"
+
+  for (let i = 0; i < morse_words.length; i++) {
+    morse_sign = morse_words[i].split(""); //creates an array with all the signs of the word
+    //creating an array consisting of all the parts plus after each morse_sign array adding a space element to see where to set the break for the longer break
+    for (let j = 0; j < morse_sign.length; i++) {}
+  }
+
+  Tone.start();
 }
